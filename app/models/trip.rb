615bda455e496636e67a814e4087fa2cf30ae2cb
satomi46 <satohat@gmail.com>
2022-12-09 20:40:21 +0900

    バリデーションの追加と旅行計画作成機能の単体テストコードの記述

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,6 +3,13 @@ class Trip < ApplicationRecord
   validates :title,      presence: true
   validates :start_date, presence: true
   validates :end_date,   presence: true
+  validate  :start_end_check
+
+  def start_end_check
+    if self.start_date != nil && self.end_date != nil 
+      errors.add(:end_date, 'must be same or later than Start date') unless self.start_date <= self.end_date 
+    end
+  end
 
   has_many :trip_users
   has_many :users, through: :trip_users

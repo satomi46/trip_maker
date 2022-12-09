4888e7afe61e6c7ad4fe4439e7f2779692b880e9
satomi46 <satohat@gmail.com>
2022-12-09 19:55:09 +0900

    バリデーションの設定、ブラウザ上でのエラーメッセージの表示

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,9 @@
 class Trip < ApplicationRecord
+  validates :image,      presence: true
+  validates :title,      presence: true
+  validates :start_date, presence: true
+  validates :end_date,   presence: true
+
   has_many :trip_users
   has_many :users, through: :trip_users
   has_one_attached :image

693498754c29574e32fccb2cc23e81afda83f06d
satomi46 <satohat@gmail.com>
2022-12-10 22:46:08 +0900

    マイページのビューの作成（旅行計画一覧表示）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
+  before_action :move_to_index, only: :show
+
   def show
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @trips = user.trips
+  end
+  
+  private
+  def move_to_index
     redirect_to root_path unless user_signed_in? && current_user.id == params[:id].to_i
   end
 end

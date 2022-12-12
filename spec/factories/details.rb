4b71f3c277ebda89130e139c0105e8d585f4ae64
satomi46 <satohat@gmail.com>
2022-12-12 23:42:59 +0900

    テストコードの記述と実行

--- spec/factories/details.rb
+++ spec/factories/details.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :detail do
-    
+    title      { Faker::Team.name }
+    note       { Faker::Lorem.sentence }
+    address    { Faker::Lorem.sentence }
+    time       { Faker::Date.between(from: '2022-01-01', to: '2030-12-31') }
+    time_note  { Faker::Lorem.sentence }
+    importance { Faker::Number.between(from: 1, to: 5) }
+    url        { Faker::Internet.url }
+    association :trip
   end
 end
--- spec/models/detail_spec.rb
+++ spec/models/detail_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Detail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @detail = FactoryBot.build(:detail)
+  end
+
+  describe '詳細スケジュール作成' do
+    context '新規作成できる場合' do
+      it 'title, importanceがあれば作成できる' do
+        expect(@detail).to be_valid
+      end
+    end
+
+    context '新規作成できない場合' do
+      it 'titleが空だとできない' do
+        @detail.title = ''
+        @detail.valid?
+        expect(@detail.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it 'importanceが空だとできない' do
+        @detail.importance = ''
+        @detail.valid?
+        expect(@detail.errors.full_messages).to include("Importance can't be blank")
+      end
+    end
+  end
 end

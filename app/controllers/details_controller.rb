f3acf56d2eb491b96a001f9d6bbfab42ee0f6e27
satomi46 <satohat@gmail.com>
2022-12-13 17:29:08 +0900

    詳細スケジュールの編集・削除機能の実装

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -10,6 +10,27 @@ class DetailsController < ApplicationController
     end
   end
 
+  def edit
+    @trip = Trip.find(params[:trip_id])
+    @detail = Detail.find(params[:id])
+  end
+
+  def update
+    @detail = Detail.find(params[:id])
+    if @detail.update(detail_params)
+      redirect_to trip_path(params[:trip_id])
+    else
+      @trip = Trip.find(params[:trip_id])
+      render 'edit'
+    end
+  end
+
+  def destroy
+    detail = Detail.find(params[:id])
+    detail.destroy
+    redirect_to trip_path(params[:trip_id])
+  end
+
   private
   def detail_params
     params.require(:detail).permit(:title, :note, :address, :time, :time_note, :importance, :url, :fixed).merge(trip_id: params[:trip_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "trips#index"
   resources :trips do
-    resources :details, only: [:create, :edit, :destroy]
+    resources :details, only: [:create, :edit, :update, :destroy]
   end
   resources :users, only: :show
 end
